1c66e7d9f92b0efb2a435aa49aacd702467716ac
6042192 <Tuhin.SubhraDe@thomsonreuters.com>
2017-04-19 16:51:59 +0530

    Test2 and Test3 updated

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -6,5 +6,6 @@ package com.test;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test2");
+
     }
 }
--- src/com/test/Test3.java
+++ src/com/test/Test3.java
@@ -4,4 +4,7 @@ package com.test;
  * Created by U6042192 on 18-Apr-17.
  */
 public class Test3 {
+    public static void main(String[] args) {
+        
+    }
 }
